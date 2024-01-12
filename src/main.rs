afca6d0e7acb604a3d1dd5fbdc2ea2264f501551
LePretzel <vince0782@gmail.com>
2024-01-12 15:19:59 -0600

    Make it so map is read from file

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,45 @@
 mod player;
 
-use std::time::{Duration, Instant};
+use std::{fs::File, io::Read, path::Path, time::Instant};
 
 use player::Player;
-use sdl2::{
-    event::Event,
-    keyboard::Keycode,
-    pixels::Color,
-    rect::Point,
-    sys::{KeyCode, SDL_Keycode, Screen},
-};
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
 
 use crate::player::Side;
 
 pub const SCREEN_WIDTH: u32 = 1280;
 pub const SCREEN_HEIGHT: u32 = 720;
 
+fn read_map() -> Vec<Vec<i32>> {
+    // Assumes that map in file is rectangular
+    let path = Path::new("src/map.txt");
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("can't open map file: {}", why),
+        Ok(file) => file,
+    };
+    let mut map: Vec<Vec<i32>> = Vec::new();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    for line in contents.lines() {
+        let mut row: Vec<i32> = Vec::new();
+        row.push(1); // To ensure the map has surrounding walls
+        for char in line.chars() {
+            if char.is_numeric() {
+                row.push(char.to_digit(10).unwrap() as i32);
+            }
+        }
+        row.push(1); // To ensure the map has surrounding walls
+        map.push(row);
+    }
+    // Add a row of ones to top and bottom of map to ensure surrounding walls
+    map.insert(0, vec![1; map[0].len() + 2]);
+    map.push(vec![1; map[0].len() + 2]);
+    map
+}
+
 fn main() {
     let mut player = Player::at_position(1.5, 1.5);
-    fn simple_map() -> Vec<Vec<i32>> {
-        vec![
-            vec![1, 1, 1, 1, 1],
-            vec![1, 0, 0, 0, 1],
-            vec![1, 0, 0, 0, 1],
-            vec![1, 1, 1, 1, 1],
-        ]
-    }
-    let map = simple_map();
+    let map = read_map();
 
     #[derive(Debug)]
     enum MoveType {
